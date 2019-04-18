ed236ed39a17924789d3721274838687bd6c3ea2
Nguyen Anh <miyamoto.anh@gmail.com>
2019-04-18 21:58:20 +0700

    cache_manifest

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :phoenix_crawler, PhoenixCrawlerWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "phoenixcrawler.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
